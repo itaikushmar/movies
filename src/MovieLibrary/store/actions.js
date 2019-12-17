b4adaf3b9118454c27e50de9809c52501954be1a
Itai Kushmar <itaikushmar@gmail.com>
2019-12-18 00:22:59 +0200

    TASK 3 - 3 pages initial view

--- src/MovieLibrary/store/actions.js
+++ src/MovieLibrary/store/actions.js
@@ -4,11 +4,15 @@ import Axios from 'axios';
 
 export const fetchTopRatedMovies = () => async dispatch => {
   const API_KEY = '54ffed57deb5a7a8688be4de3007e578';
-  const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
-
-  const { data } = await Axios.get(URL);
+  const numberOfPages = 3;
+  let response = [];
+  for (var i=1; i<=numberOfPages; i++) {
+    const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&page=${i}`;
+    const { data } = await Axios.get(URL);
+    response.push(...data.results);
+  }
   dispatch({
     type: FETCH_MOVIES,
-    payload: data.results
+    payload: response
   });
 };
