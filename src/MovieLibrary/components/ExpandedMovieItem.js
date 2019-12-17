d9d9e58547b15f049c3843f66e54faf675438790
Itai Kushmar <itaikushmar@gmail.com>
2019-12-17 23:04:05 +0200

    TASK 1 - Movie list and Modal

--- src/MovieLibrary/components/ExpandedMovieItem.js
+++ src/MovieLibrary/components/ExpandedMovieItem.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-import TMDBImage from './TMDBImage';;
+import TMDBImage from './TMDBImage';
+import './ExpandedMovieItem.css';
 
-const ExpandedMovieItem = ({movie: {title, original_title, poster_path, overview, vote_average, vote_count}}) => (
+const ExpandedMovieItem = ({movie: {title, original_title, poster_path, overview, vote_average, vote_count, release_date}}) => {
+  const releaseDate = new Date(release_date).toLocaleDateString();
+  const movieTitle = original_title === title ? title : `${title} (${original_title})`;
+  return (
     <div className="expanded-movie-item">
       <TMDBImage src={poster_path} className="poster" />
       <div className="description">
-        <h2>{title}({original_title})</h2>
-        <div><h4>Rank(votes count)</h4>: <span>{vote_average}({vote_count})</span></div>
+      <div className="movie_title_section">
+      <h2 className="movie_title">{movieTitle}</h2>
+      <h5 className="movie_subTitle">{releaseDate} | {vote_average} ({vote_count} votes)</h5>
+      </div>
+        <div className="overview">
         <span>{overview}</span>
+        </div>
       </div>
     </div>
-  )
+  )}
 
   export default ExpandedMovieItem;
--- src/MovieLibrary/components/MoviesList.js
+++ src/MovieLibrary/components/MoviesList.js
@@ -8,47 +8,64 @@ import ExpandedMovieItem from './ExpandedMovieItem';
 import './MoviesList.css';
 
 class MoviesList extends React.PureComponent {
-
   static propTypes = { movies: PropTypes.array.isRequired };
 
   state = { selectedMovie: null, isModalOpen: false };
 
-  openModal = item => this.setState({selectedMovie: item, isModalOpen: true});
+  openModal = item => this.setState({ selectedMovie: item, isModalOpen: true });
 
-  closeModal = () => this.setState({selectedMovie: null, isModalOpen: false});
+  closeModal = () => this.setState({ selectedMovie: null, isModalOpen: false });
 
   handleSortingChange = sortingType => console.log(sortingType);
 
   render() {
-
-    const {movies} = this.props
-    const {selectedMovie, isModalOpen} = this.state
+    const { movies } = this.props;
+    const { selectedMovie, isModalOpen } = this.state;
+    const modalStyle = {
+      content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        width: '50%',
+        height: '50%',
+        padding: 0,
+        boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+        backgroundColor: "rgb(107, 107, 107)",
+        border: "1px solid rgb(97, 97, 97)"
+      }
+    };
 
     return (
-      <div className="movies-list">
-          <div>
-            <span>Sort by:</span>
-            <SortingOptions onChange={this.handleSortingChange}/>
-          </div>
-        <div className="items">
-          {
-            movies.map(movie =>
-              <MovieListItem key={movie.id} movie={movie} isSelected={selectedMovie===movie} onSelect={this.openModal}/>
-            )
-          }
+      <div className='movies-list'>
+        <div>
+          <span>Sort by:</span>
+          <SortingOptions onChange={this.handleSortingChange} />
         </div>
-        {
-          selectedMovie && (
-            <Modal
-              isOpen={isModalOpen}
-              onRequestClose={this.closeModal}
-            >
-              <ExpandedMovieItem movie={selectedMovie} />
-            </Modal>
-          )
-        }
+        <div className='items'>
+          {movies.map(movie => (
+            <MovieListItem
+              key={movie.id}
+              movie={movie}
+              isSelected={selectedMovie === movie}
+              onSelect={this.openModal}
+            />
+          ))}
+        </div>
+        {selectedMovie && (
+          <Modal
+            isOpen={isModalOpen}
+            onRequestClose={this.closeModal}
+            style={modalStyle}
+          >
+          <button className='close-modal' onClick={this.closeModal}>x</button>
+            <ExpandedMovieItem movie={selectedMovie} />
+          </Modal>
+        )}
       </div>
-    )
+    );
   }
 }
 
